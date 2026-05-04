615ffb041c8c966d03ef3df09dde71740d4427c9
Vishal Munde <vmunde48@gmail.com>
2026-05-05 01:00:53 +0530

    Add production config

--- frontend/vite-project/src/utils/api.js
+++ frontend/vite-project/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL || "/api",
   headers: { "Content-Type": "application/json" },
 });
 
@@ -20,7 +20,7 @@ api.interceptors.response.use(
       window.location.href = "/login";
     }
     return Promise.reject(err);
-  }
+  },
 );
 
 export default api;
